cd786743083cd00aa1ab2d8c2ba0b7e47fe40955
Mohammad Rahadian Ghifari <ghifarimohammadrahadian@gmail.com>
2023-05-10 00:54:33 +0700

    feat: add date filtering

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import NewsTable from '../components/NewsTable';
 const Dashboard = () => {
   const [data, setData] = useState([]);
   const [graph, setGraph] = useState([]);
+  const [startDate, setStartDate] = useState('2022-12-12');
+  const [endDate, setEndDate] = useState('2023-02-04');
 
   useEffect(() => {
     axios.get('http://127.0.0.1:4200/api/v1/news/graph/2022-12-12/2023-02-24/*')
@@ -12,18 +14,43 @@ const Dashboard = () => {
         setGraph(res.data);
       })
       .catch(err => console.log(err))
-    axios.get('http://127.0.0.1:4200/api/v1/news/2022-12-12/2023-02-24/*')
+  }, [])
+
+  useEffect(() => {
+    let url = `http://127.0.0.1:4200/api/v1/news`;
+    if (startDate && endDate) {
+      url += `/${startDate}/${endDate}/*`;
+    } else {
+      url += '/*';
+    }
+    axios.get(url)
       .then(res => {
         setData(res.data.data);
       })
       .catch(err => console.log(err))
-  }, [])
+  }, [startDate, endDate])
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const handleEndDateChange = (e) => {
+    setEndDate(e.target.value);
+    console.log(e.target.value);
+  }
   
   return (
     <div>
       <div className='mb-10'>
         <h1 className='text-3xl font-bold'>Dashboard</h1>
         <hr />
+      </div>
+      <div className='mb-5'>
+        <label htmlFor='startDate' className='mr-2'>Start Date:</label>
+        <input type='date' id='startDate' value={startDate} onChange={handleStartDateChange} className='border rounded-md p-1' />
+        <label htmlFor='endDate' className='ml-4 mr-2'>End Date:</label>
+        <input type='date' id='endDate' value={endDate} onChange={handleEndDateChange} className='border rounded-md p-1' />
       </div>
       <div>
         <NewsTable data={data} itemsPerPage={10}/>
